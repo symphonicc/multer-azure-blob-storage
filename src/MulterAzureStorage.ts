52fdee04d8559de2bd1a52952335ef5fe42b44b4
markmblc <29580060+markmblc@users.noreply.github.com>
2018-12-21 12:24:47 -0600

    Add contentSettings to blob upload

--- src/MulterAzureStorage.ts
+++ src/MulterAzureStorage.ts
@@ -187,7 +187,14 @@ export class MulterAzureStorage implements StorageEngine {
             // Prep stream
             let blobStream: Writable;
             if (this._metadata == null) {
-                blobStream = this._blobService.createWriteStreamToBlockBlob(containerName, blobName, (cWSTBBError, result, response) => {
+                blobStream = this._blobService.createWriteStreamToBlockBlob(containerName, blobName,
+                    {
+                        contentSettings: {
+                            contentType: file.mimetype,
+                            contentDisposition: 'inline'
+                        }
+                    }, 
+                    (cWSTBBError, result, response) => {
                     if (cWSTBBError) {
                         cb(cWSTBBError);
                     } else {
